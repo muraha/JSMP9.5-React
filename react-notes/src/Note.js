519f6dd0df68c3cc49b5c5935936d697976c6a50
KYIV\Anton_Karpenko <apknov@gmail.com>
2018-04-23 14:19:35 +0300

    Added-Icons-events-styles

--- react-notes/src/Note.js
+++ react-notes/src/Note.js
@@ -1,18 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MDEdit from 'react-icons/lib/md/edit'
+import MdDelete from 'react-icons/lib/md/delete'
+
+
 
 export default class Note extends Component {
+	constructor(props) {
+		super(props)
+		this.edit = this.edit.bind(this)
+		this.remove = this.remove.bind(this)
+	}
+	edit = () => {
+		
+	}
   
-  
-  
-  
+	remove = () => {
+		
+	}
   
   render() {
 		return (
 			<div className="note">
 				<p>Learn React</p>
 				<span>
-					<button onClick={this.edit} id="edit"><FaPencil /></button>
-					<button onClick={this.remove} id="remove"><FaTrash /></button>
+					<button onClick={this.edit} id="edit"><MDEdit /></button>
+					<button onClick={this.remove} id="remove"><MdDelete /></button>
 				</span>
 			</div>
 		)
--- react-notes/src/index.js
+++ react-notes/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import Note from './Note'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Note />, document.getElementById('root'));
 registerServiceWorker();
